f42afbe52a2ab0017e7c87e7e2f6b9ce088e4194
shadow578 <52449218+shadow578@users.noreply.github.com>
2025-01-06 12:29:20 +0100

    implement all shapes and update colors

--- src/game/tetris/tetronimo.cpp
+++ src/game/tetris/tetronimo.cpp
@@ -1,50 +1,219 @@
 #include "../game.h"
 
-// tetromino shapes, each one is a bitmap of 4 rows x 4 bits (LSB)
-// top-left corner is the x/y position recorded
-const uint8_t TetrisGame::TETROMINO_SHAPES[/*id*/ 7][/*rotation*/ 4][/*row*/ 4] = {
-    // I
-    {
-        // 0 degrees
-        {
-            0b1000,
-            0b1000,
-            0b1000,
-            0b1000,
-        },
-        // 90 degrees
-        {
-            0b1111,
-            0b0000,
-            0b0000,
-            0b0000,
-        },
-        // 180 degrees
-        {
-            0b1000,
-            0b1000,
-            0b1000,
-            0b1000,
-        },
-        // 270 degrees
-        {
-            0b1111,
-            0b0000,
-            0b0000,
-            0b0000,
-        },
-    }
+// Shapes, rotations and colors according to https://tetris.fandom.com/wiki/SRS?file=SRS-pieces.png
 
-    // TODO other shapes
+/**
+ * Tetromino shapes, each one being a bitmap of 4 rows x 4 bits
+ * The top-left corner (row 0, bit 4) is the origin of the shape
+ */
+const uint8_t TetrisGame::TETROMINO_SHAPES[/*id*/ 7][/*rotation*/ 4][/*row*/ 4] = {
+  { // I
+    { // 0 degrees
+      0b0000,
+      0b1111,
+      0b0000,
+      0b0000,
+    },
+
+    { // 90 degrees
+      0b0010,
+      0b0010,
+      0b0010,
+      0b0010,
+    },
+
+    { // 180 degrees
+      0b0000,
+      0b0000,
+      0b1111,
+      0b0000,
+    },
+    { // 270 degrees
+      0b0100,
+      0b0100,
+      0b0100,
+      0b0100,
+    },
+  },
+  { // J
+    { // 0 degrees
+      0b1000,
+      0b1110,
+      0b0000,
+      0b0000,
+    },
+
+    { // 90 degrees
+      0b0110,
+      0b0100,
+      0b0100,
+      0b0000,
+    },
+
+    { // 180 degrees
+      0b0000,
+      0b1110,
+      0b0010,
+      0b0000,
+    },
+    { // 270 degrees
+      0b0100,
+      0b0100,
+      0b1100,
+      0b0000,
+    },
+  },
+  { // L
+    { // 0 degrees
+      0b0010,
+      0b1110,
+      0b0000,
+      0b0000,
+    },
+
+    { // 90 degrees
+      0b0100,
+      0b0100,
+      0b0110,
+      0b0000,
+    },
+
+    { // 180 degrees
+      0b0000,
+      0b1110,
+      0b1000,
+      0b0000,
+    },
+    { // 270 degrees
+      0b1100,
+      0b0100,
+      0b0100,
+      0b0000,
+    },
+  },
+  { // O
+    { // 0 degrees
+      0b1100,
+      0b1100,
+      0b0000,
+      0b0000,
+    },
+
+    { // 90 degrees
+      0b1100,
+      0b1100,
+      0b0000,
+      0b0000,
+    },
+
+    { // 180 degrees
+      0b1100,
+      0b1100,
+      0b0000,
+      0b0000,
+    },
+    { // 270 degrees
+      0b1100,
+      0b1100,
+      0b0000,
+      0b0000,
+    },
+  },
+  { // S
+    { // 0 degrees
+      0b0110,
+      0b1100,
+      0b0000,
+      0b0000,
+    },
+
+    { // 90 degrees
+      0b0100,
+      0b0110,
+      0b0010,
+      0b0000,
+    },
+
+    { // 180 degrees
+      0b0000,
+      0b0110,
+      0b1100,
+      0b0000,
+    },
+    { // 270 degrees
+      0b1000,
+      0b1100,
+      0b0100,
+      0b0000,
+    },
+  },
+  { // T
+    { // 0 degrees
+      0b0100,
+      0b1110,
+      0b0000,
+      0b0000,
+    },
+
+    { // 90 degrees
+      0b0100,
+      0b0110,
+      0b0100,
+      0b0000,
+    },
+
+    { // 180 degrees
+      0b0000,
+      0b1110,
+      0b0100,
+      0b0000,
+    },
+    { // 270 degrees
+      0b0100,
+      0b1100,
+      0b0100,
+      0b0000,
+    },
+  },
+  { // Z 
+    { // 0 degrees
+      0b1100,
+      0b0110,
+      0b0000,
+      0b0000,
+    },
+
+    { // 90 degrees
+      0b0010,
+      0b0110,
+      0b0100,
+      0b0000,
+    },
+
+    { // 180 degrees
+      0b0000,
+      0b1100,
+      0b0110,
+      0b0000,
+    },
+    { // 270 degrees
+      0b0100,
+      0b1100,
+      0b1000,
+      0b0000,
+    },
+  }
 };
 
-// tetromino colors
+/**
+ * Colors of the tetrominos
+ */
 const TetrisGame::color TetrisGame::TETROMINO_COLORS[7] = {
-    color::RED,     // I
-    color::BLUE,    // J
-    color::WHITE,   // L
-    color::YELLOW,  // O
-    color::GREEN,   // S
-    color::MAGENTA, // T
-    color::CYAN,    // Z
+  color::CYAN,    // I
+  color::BLUE,    // J
+  color::WHITE,   // L
+  color::YELLOW,  // O
+  color::GREEN,   // S
+  color::MAGENTA, // T
+  color::RED,     // Z
 };
